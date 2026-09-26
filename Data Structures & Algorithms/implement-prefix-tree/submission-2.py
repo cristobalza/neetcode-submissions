5d4a847f5335a4f318dc11d3629eb11c1def6f6e
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2026-09-25 20:47:36 -0500

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, isEnd: bool = False):
+        self.isEnd = isEnd
+        self.children = {} # letter to list
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node()
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+    
+            if ch not in curr.children:
+                curr.children[ch] = Node()
+
+            curr = curr.children[ch]
+
+        curr.isEnd = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for ch in word:
+
+            if ch not in curr.children:
+                return False
+
+            curr = curr.children[ch]
+
+        return curr.isEnd
+        
+
+    def startsWith(self, prefix: str) -> bool:
+
+        curr = self.root
+
+        for ch in prefix:
+
+            if ch not in curr.children:
+                return False
+
+            curr = curr.children[ch]
+
+        return True
+        
+        
